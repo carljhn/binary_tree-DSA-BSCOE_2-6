fd6a722c2a0c90353f06e12e8437ed2809ca9129
Carljhn <zoletacarljohn1@gmail.com>
2023-01-14 14:14:02 -1200

    Fifth commit: Added the function delete.

--- exercise_p2.py
+++ exercise_p2.py
@@ -46,3 +46,24 @@ class BinarySearchTreeNode:
             elements += self.right.in_order_traversal()
 
         return elements
+
+    def delete(self, val):
+        if val < self.data:
+            if self.left:
+                self.left = self.left.delete(val)
+        elif val > self.data:
+            if self.right:
+                self.right = self.right.delete(val)
+        else:
+            if self.left is None and self.right is None:
+                return None
+            elif self.left is None:
+                return self.right
+            elif self.right is None:
+                return self.right
+
+            max_val = self.left.find_max()
+            self.data = max_val
+            self.left = self.left.delete(max_val)
+
+        return self
