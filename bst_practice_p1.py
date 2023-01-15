0fb1d0af3b2820a3dbf10810fba50c2113120943
Carljhn <zoletacarljohn1@gmail.com>
2023-01-14 12:00:12 -1200

    Third commit: Defined a function called in_order_traversal that arranges the elements.

--- bst_practice_p1.py
+++ bst_practice_p1.py
@@ -23,4 +23,19 @@ class BinarySearchTreeNode:
                 self.right.add_child(data)
             else:
                 self.right = BinarySearchTreeNode(data)
-                
+    
+    def in_order_traversal(self):
+        elements = []
+        
+        # visits left tree
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        # visits the base node
+        elements.append(self.data)
+
+        # visits right tree
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements
