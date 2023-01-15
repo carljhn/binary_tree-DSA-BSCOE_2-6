5cfa0fbb1ddeca98ce427d440b4ef8b7ebd6a9ef
Carljhn <zoletacarljohn1@gmail.com>
2023-01-14 13:01:59 -1200

    Final commit: Fished the program with an if statement and a sample list.

--- exercise_p1.py
+++ exercise_p1.py
@@ -89,3 +89,17 @@ def build_tree(elements):
         root.add_child(elements[i])
 
     return root
+
+if __name__ == '__main__':
+    numbers = [17, 4, 1, 20, 9, 23, 18, 34]
+
+    numbers = [15, 12, 7, 14, 27, 20, 23, 88]
+
+    numbers_tree = build_tree(numbers)
+    print("Input numbers:", numbers)
+    print("Min:", numbers_tree.find_min())
+    print("Max:", numbers_tree.find_max())
+    print("Sum:", numbers_tree.calculate_sum())
+    print("In order traversal:", numbers_tree.in_order_traversal())
+    print("Pre order traversal:", numbers_tree.pre_order_traversal())
+    print("Post order traversal:", numbers_tree.post_order_traversal())
